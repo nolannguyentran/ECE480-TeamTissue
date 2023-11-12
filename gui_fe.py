f7f5b6a27b9c4c411927a0f256463e07fc8cf639
nolannguyentran <nolannguyentran@u.boisestate.edu>
2023-11-12 16:31:23 -0700

    added navigation buttons

--- gui_fe.py
+++ gui_fe.py
@@ -14,9 +14,6 @@ class MyFrame(wx.Frame):
        def __init__(self, parent, ID, title):
         wx.Frame.__init__(self, parent, ID, title, size=(length, width))
 
-        
-
-    
         panel_1 = wx.Panel(self, -1, style=wx.BORDER_RAISED)
         panel_2 = wx.Panel(self, -1, style=wx.BORDER_RAISED)
         panel_3 = wx.Panel(self, -1, style=wx.BORDER_RAISED)
@@ -32,23 +29,41 @@ class MyFrame(wx.Frame):
         button_c = wx.Button(panel_3, wx.ID_ANY, 'Motor C')
         button_d = wx.Button(panel_3, wx.ID_ANY, 'Motor D')
 
+        button_home = wx.Button(panel_4, wx.ID_ANY, 'Dashboard')
+        button_jobs = wx.Button(panel_4, wx.ID_ANY, 'Jobs')
+        button_settings = wx.Button(panel_4, wx.ID_ANY, 'Settings')
+
+        button_a.SetBackgroundColour((89, 99, 182))
+        button_b.SetBackgroundColour((89, 99, 182))
+        button_c.SetBackgroundColour((89, 99, 182))
+        button_d.SetBackgroundColour((89, 99, 182))
+        button_home.SetBackgroundColour((0, 0, 0))
+        button_jobs.SetBackgroundColour((0, 0, 0))
+        button_settings.SetBackgroundColour((0, 0, 0))
+
         text_1 = wx.StaticText(panel_1, label = now, style=wx.ALIGN_RIGHT)
    
         window_sizer = wx.BoxSizer(wx.VERTICAL)           #For housing entire application window 
         middle_sizer = wx.BoxSizer(wx.HORIZONTAL)         #For housing middle panel
-        grid_sizer = wx.GridSizer(2, 2, 5, 5)             #For housing the four motor buttons
+        motor_grid_sizer = wx.GridSizer(2, 2, 10, 10)     #For housing the four motor buttons
+        navigation_grid_sizer = wx.GridSizer(1, 3, 5, 5)  #For housig the three navigation buttons
 
         middle_sizer.Add(panel_2, 1, wx.EXPAND)
         middle_sizer.Add(panel_3, 1, wx.EXPAND)
 
-        grid_sizer.Add(button_a, 0, wx.EXPAND)
-        grid_sizer.Add(button_b, 0, wx.EXPAND)
-        grid_sizer.Add(button_c, 0, wx.EXPAND)
-        grid_sizer.Add(button_d, 0, wx.EXPAND)
+        motor_grid_sizer.Add(button_a, 0, wx.EXPAND)
+        motor_grid_sizer.Add(button_b, 0, wx.EXPAND)
+        motor_grid_sizer.Add(button_c, 0, wx.EXPAND)
+        motor_grid_sizer.Add(button_d, 0, wx.EXPAND)
 
-        panel_3.SetSizer(grid_sizer)
+        navigation_grid_sizer.Add(button_home, 0, wx.EXPAND)
+        navigation_grid_sizer.Add(button_jobs, 0, wx.EXPAND)
+        navigation_grid_sizer.Add(button_settings, 0, wx.EXPAND)
         
 
+        panel_3.SetSizer(motor_grid_sizer)
+        panel_4.SetSizer(navigation_grid_sizer)
+        
         window_sizer.Add(panel_1, 1, wx.EXPAND)
         window_sizer.Add(middle_sizer, 10, wx.EXPAND)
         window_sizer.Add(panel_4, 2, wx.EXPAND)
