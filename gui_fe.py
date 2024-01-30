4a1e106d134d3da0f9756a81aadd0338b16bf9b5
Nolan Nguyen-Tran <nolannguyentran@u.boisestate.edu>
2024-01-30 02:18:23 -0700

    added fourth frame, delete back-end code

--- gui_fe.py
+++ gui_fe.py
@@ -14,19 +14,25 @@ def on_motor_click(event):
 
 def on_compression_test_click(event, motor_name):
     #print("compression test selected")
-    identity = event.GetEventObject().GetLabel()    #Returns which capsule selected
+    identity = event.GetEventObject().GetLabel()    #Returns which test selected
     frame_2.Destroy()
+    global frame_3
     frame_3 = MyThirdFrame(motor_name, identity)
     frame_3.Show()
 
 def on_tensile_test_click(event, motor_name):
     #print("tensile test selected")
-    identity = event.GetEventObject().GetLabel()    #Returns which capsule selected
+    identity = event.GetEventObject().GetLabel()    #Returns which test selected
     frame_2.Destroy()
+    global frame_3
     frame_3 = MyThirdFrame(motor_name, identity)
     frame_3.Show()
-   
 
+def on_start_test_click(event, motor_name, test_name):
+    identity = event.GetEventObject().GetLabel()
+    frame_3.Destroy()
+    frame_4 = MyFourthFrame(motor_name, test_name)
+    frame_4.Show()
 
 class MyFrame(wx.Frame):
     def __init__(self, parent, ID, title):
@@ -104,7 +110,7 @@ class MySecondFrame(wx.Frame):
        def __init__(self, name):
         wx.Frame.__init__(self, None, size=(length, width))
 
-        self.name = name
+        self.name = name                    #Capsule Name
         dashboard_img = wx.Bitmap("./pictures/dashboard.png")
         jobs_img = wx.Bitmap("./pictures/jobs.png")
         settings_img = wx.Bitmap("./pictures/settings.png")
@@ -196,7 +202,7 @@ class MyThirdFrame(wx.Frame):
         wx.Frame.__init__(self, None, size=(length, width))
 
         self.motor_name = motor_name
-        self.name = name
+        self.name = name                    #Test type name
         dashboard_img = wx.Bitmap("./pictures/dashboard.png")
         jobs_img = wx.Bitmap("./pictures/jobs.png")
         settings_img = wx.Bitmap("./pictures/settings.png")
@@ -238,6 +244,8 @@ class MyThirdFrame(wx.Frame):
 
         button_start = wx.Button(panel_4, wx.ID_ANY, 'START')
         button_start.SetBackgroundColour((190, 37, 66))
+
+        button_start.Bind(wx.EVT_BUTTON, lambda event: on_start_test_click(event, self.motor_name, self.name))
 
         button_home = wx.BitmapButton(panel_5, wx.ID_ANY, bitmap = dashboard_img)
         button_jobs = wx.BitmapButton(panel_5, wx.ID_ANY, bitmap = jobs_img)
@@ -282,8 +290,86 @@ class MyThirdFrame(wx.Frame):
         self.SetSizer(window_sizer)
         self.Layout()
 
+class MyFourthFrame(wx.Frame):
+    def __init__(self, motor_name, name):
+        wx.Frame.__init__(self, None, size=(length, width))
+
+        self.motor_name = motor_name
+        self.name = name
+        dashboard_img = wx.Bitmap("./pictures/dashboard.png")
+        jobs_img = wx.Bitmap("./pictures/jobs.png")
+        settings_img = wx.Bitmap("./pictures/settings.png")
+
+        panel_1 = wx.Panel(self, -1,)       #For housing name, date, and time
+        panel_2 = wx.Panel(self, -1,)       #For housing graph
+        panel_3 = wx.Panel(self, -1,)       #For housing capsule name, test, and export button
+        panel_4 = wx.Panel(self, -1,)       #For housing navigation buttons
+    
+        panel_1.SetBackgroundColour((53, 62, 108))        
+        panel_2.SetBackgroundColour((33, 37, 41))         
+        panel_3.SetBackgroundColour((33, 37, 41))
+        panel_4.SetBackgroundColour((53, 62, 108))               
+
+        font_1 = wx.Font(20, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        font_2 = wx.Font(14, wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+        
+        t_0 = wx.StaticText(panel_1, label = "BioReact")    #tmp placeholder for future photoshopped trademark
+        t_1 = wx.StaticText(panel_1, label = now)
+        t_2 = wx.StaticText(panel_2, label = self.motor_name+ " - "+ self.name) 
+        t_2.SetFont(font_1)
+        
+        text_sizer_1 = wx.BoxSizer(wx.HORIZONTAL)     #Aligning date and time right
+        text_sizer_1.Add(t_0, 1, wx.EXPAND)
+        text_sizer_1.Add((0,0), 2, wx.ALIGN_CENTER)
+        text_sizer_1.Add(t_1, 0, wx.EXPAND)
+
+        text_sizer_2 = wx.BoxSizer(wx.HORIZONTAL)   #Aligning Capsule Name and type of test in center
+        text_sizer_2.Add((0,0), 1, wx.EXPAND)
+        text_sizer_2.Add(t_2, 0, wx.ALIGN_CENTER)
+        text_sizer_2.Add((0,0), 1, wx.EXPAND)
+
+        #button_start = wx.Button(panel_4, wx.ID_ANY, 'START')
+        #button_start.SetBackgroundColour((190, 37, 66))
+
+        button_home = wx.BitmapButton(panel_4, wx.ID_ANY, bitmap = dashboard_img)
+        button_jobs = wx.BitmapButton(panel_4, wx.ID_ANY, bitmap = jobs_img)
+        button_settings = wx.BitmapButton(panel_4, wx.ID_ANY, bitmap = settings_img)
+        
+        button_home.SetBackgroundColour((0, 0, 0))
+        button_jobs.SetBackgroundColour((0, 0, 0))
+        button_settings.SetBackgroundColour((0, 0, 0))
+
+        window_sizer = wx.BoxSizer(wx.VERTICAL)           #For housing entire application window 
+        middle_sizer = wx.BoxSizer(wx.HORIZONTAL)         #For housing middle panel
+
+     
+
+        middle_sizer.Add(panel_2, 1, wx.EXPAND)
+        middle_sizer.Add(panel_3, 1, wx.EXPAND)
+       
+        navigation_grid_sizer = wx.GridSizer(1, 3, 0, 0)  #For housig the three navigation buttons
+        navigation_grid_sizer.Add(button_home, 0, wx.EXPAND)
+        navigation_grid_sizer.Add(button_jobs, 0, wx.EXPAND)
+        navigation_grid_sizer.Add(button_settings, 0, wx.EXPAND)
+        
+        panel_1.SetSizer(text_sizer_1)
+        panel_2.SetSizer(text_sizer_2)
+        #panel_3.SetSizer(user_field_sizer)
+        panel_4.SetSizer(navigation_grid_sizer)
+    
+
+        window_sizer.Add(panel_1, 1, wx.EXPAND)
+        #window_sizer.Add(panel_2, 2, wx.EXPAND)
+        window_sizer.Add(middle_sizer, 10, wx.EXPAND)
+        window_sizer.Add(panel_4, 2, wx.EXPAND)
+   
+        self.SetAutoLayout(True)
+        self.SetSizer(window_sizer)
+        self.Layout()
+
    
 app = wx.App(False)
-frame = MyFrame(None, -1, "BioReactor")
-frame.Show()
+global home_frame
+home_frame = MyFrame(None, -1, "BioReactor")
+home_frame.Show()
 app.MainLoop()
