d103ab45412c61f29c523639dcb268de14b3c4e1
Jason Pearson <pearsonjason2000@gmail.com>
2024-02-16 17:36:55 -0700

    Change motor GPIO pins

--- motor.py
+++ motor.py
@@ -3,32 +3,32 @@ import RPi.GPIO as GPIO
 
 
 #Config for stepper motor 1
-DIR = 20        # Direction GPIO Pin
-STEP = 21       # Step GPIO Pin
+DIR_PIN_S1 = 29        # Direction GPIO Pin motor 1
+STEP_PIN_S1 = 27       # Step GPIO Pin motor 1 
 CW = 1          # Clockwise 
 CCW = 0         # CounterClockwise
 SPR = 202       # Steps per Revolution (365 / 1.8)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)    #set initial rotation to cw
+GPIO.setup(DIR_PIN_S1, GPIO.OUT)
+GPIO.setup(STEP_PIN_S1, GPIO.OUT)
+GPIO.output(DIR_PIN_S1, CW)    #set initial rotation to cw
 
 step_count = SPR    #rotation
 delay = 0.005       # 1 sec / 202
 
 for i in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
+    GPIO.output(STEP_PIN_S1, GPIO.HIGH)
     sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
+    GPIO.output(STEP_PIN_S1, GPIO.LOW)
     sleep(delay)
 
 sleep(0.5)
-GPIO.output(DIR, CCW)   #set rotation to ccw
+GPIO.output(DIR_PIN_S1, CCW)   #set rotation to ccw
 for i in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
+    GPIO.output(STEP_PIN_S1, GPIO.HIGH)
     sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
+    GPIO.output(STEP_PIN_S1, GPIO.LOW)
     sleep(delay)
 
 
