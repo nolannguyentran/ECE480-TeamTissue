c30840585bb282f17b6cb0c35d7792a96dbb7001
nolannguyentran <nolannguyentran@u.boisestate.edu>
2024-03-06 22:08:49 -0700

    added strain type input page

--- gui_fe.py
+++ gui_fe.py
@@ -1,5 +1,5 @@
 import wx
-import gui_be
+#import gui_be
 
 from datetime import datetime
 length = 800
@@ -26,20 +26,34 @@ def on_motor_click(event):
 def on_compression_test_click(event, motor_name):
     identity = event.GetEventObject().GetLabel()    #Returns which test selected
     test_selection_frame.Destroy()
-    global strain_rate_frame
-    strain_rate_frame = TestInput(motor_name, identity)
-    strain_rate_frame.Show()
+    global strain_input_type_frame
+    strain_input_type_frame = StrainInputTypeFrame(motor_name, identity)
+    strain_input_type_frame.Show()
+    #global strain_rate_frame
+    #strain_rate_frame = TestInput(motor_name, identity)
+    #strain_rate_frame.Show()
 
 def on_tensile_test_click(event, motor_name):
     identity = event.GetEventObject().GetLabel()    #Returns which test selected
     test_selection_frame.Destroy()
-    global strain_rate_frame
-    strain_rate_frame = TestInput(motor_name, identity)
-    strain_rate_frame.Show()
+    global strain_input_type_frame
+    strain_input_type_frame = StrainInputTypeFrame(motor_name, identity)
+    strain_input_type_frame.Show()
+    #global strain_rate_frame
+    #strain_rate_frame = TestInput(motor_name, identity)
+    #strain_rate_frame.Show()
+
+def on_constant_test_click(event, motor_name):
+    identity = event.GetEventObject().GetLabel()    #Returns which test input type selected (constant rate)
+    strain_input_type_frame.Destroy()
+    global constant_strain_test_frame
+    constant_strain_test_frame = ConstantStrainTestInput(motor_name, identity)
+    constant_strain_test_frame.Show()
+
 
 def on_start_test_click(event, motor_name, test_name):
     identity = event.GetEventObject().GetLabel()
-    strain_rate_frame.Destroy()
+    constant_strain_test_frame.Destroy()
     global live_test_frame
     live_test_frame = TestOutput(motor_name, test_name)
     live_test_frame.Show()
@@ -83,9 +97,13 @@ class HomeFrame(wx.Frame):
         button_home.Disable()
 
         button_a.SetBackgroundColour((89, 99, 182))
+        button_a.SetForegroundColour((255,255,255))
         button_b.SetBackgroundColour((89, 99, 182))
+        button_b.SetForegroundColour((255,255,255))
         button_c.SetBackgroundColour((89, 99, 182))
+        button_c.SetForegroundColour((255,255,255))
         button_d.SetBackgroundColour((89, 99, 182))
+        button_d.SetForegroundColour((255,255,255))
         button_home.SetBackgroundColour((0, 0, 0))
         button_jobs.SetBackgroundColour((0, 0, 0))
         button_settings.SetBackgroundColour((0, 0, 0))
@@ -191,7 +209,9 @@ class TestSelectionFrame(wx.Frame):
         button_tensile = wx.Button(panel_3, wx.ID_ANY, 'Tensile Test')
 
         button_compression.SetBackgroundColour((89, 99, 182))
+        button_compression.SetForegroundColour((255,255,255))
         button_tensile.SetBackgroundColour((89, 99, 182))
+        button_tensile.SetForegroundColour((255,255,255))
 
         button_compression.Bind(wx.EVT_BUTTON, lambda event: on_compression_test_click(event, self.name))
         button_tensile.Bind(wx.EVT_BUTTON, lambda event: on_tensile_test_click(event, self.name))
@@ -235,7 +255,112 @@ class TestSelectionFrame(wx.Frame):
         self.SetSizer(window_sizer)
         self.Layout()
 
-class TestInput(wx.Frame):
+class StrainInputTypeFrame(wx.Frame):
+       def __init__(self, motor_name, test_type):
+        wx.Frame.__init__(self, None, size=(length, width))
+
+        self.name = motor_name                    #Capsule Name
+        self.test_type = test_type
+        dashboard_img = wx.Bitmap("./pictures/dashboard.png")
+        jobs_img = wx.Bitmap("./pictures/jobs.png")
+        settings_img = wx.Bitmap("./pictures/settings.png")
+
+        panel_1 = wx.Panel(self, -1,)       #For housing name, date, and time
+        panel_2 = wx.Panel(self, -1,)       #For capsule name
+        panel_3 = wx.Panel(self, -1,)       #For housing two test selection buttons
+        panel_4 = wx.Panel(self, -1,)       #For housing instructions
+        panel_5 = wx.Panel(self, -1,)       #For housing navigation buttons
+    
+        panel_1.SetBackgroundColour((53, 62, 108))        
+        panel_2.SetBackgroundColour((33, 37, 41))         
+        panel_3.SetBackgroundColour((33, 37, 41))
+        panel_4.SetBackgroundColour((33, 37, 41))               
+        panel_5.SetBackgroundColour((53, 62, 108))
+
+        font_1 = wx.Font(20, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        font_2 = wx.Font(14, wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+        
+        t_0 = wx.StaticText(panel_1, label = "BioReact")    #tmp placeholder for future photoshopped trademark
+        t_0.SetForegroundColour((255, 255, 255))
+        t_1 = wx.StaticText(panel_1, label = now)
+        t_1.SetForegroundColour((255, 255, 255))
+        t_2 = wx.StaticText(panel_2, label = self.name) 
+        t_2.SetFont(font_1)
+        t_2.SetForegroundColour((255, 255, 255))
+        t_3 = wx.StaticText(panel_4, label = "Please select test input type to conduct with "+self.name+ " - "+ self.test_type)
+        t_3.SetFont(font_2)
+        t_3.SetForegroundColour((255, 255, 255))
+      
+        text_sizer_1 = wx.BoxSizer(wx.HORIZONTAL)     #Aligning date and time right
+        text_sizer_1.Add(t_0, 1, wx.EXPAND)
+        text_sizer_1.Add((0,0), 2, wx.ALIGN_CENTER)
+        text_sizer_1.Add(t_1, 0, wx.EXPAND)
+
+        text_sizer_2 = wx.BoxSizer(wx.HORIZONTAL)   #Aligning Capsule Name in center
+        text_sizer_2.Add((0,0), 1, wx.EXPAND)
+        text_sizer_2.Add(t_2, 0, wx.ALIGN_CENTER)
+        text_sizer_2.Add((0,0), 1, wx.EXPAND)
+
+        text_sizer_3 = wx.BoxSizer(wx.HORIZONTAL)   #Aligning instructions in center
+        text_sizer_3.Add((0,0), 1, wx.EXPAND)
+        text_sizer_3.Add(t_3,0,wx.ALIGN_CENTER)
+        text_sizer_3.Add((0,0), 1, wx.EXPAND)
+
+        button_constant = wx.Button(panel_3, wx.ID_ANY, 'Constant Strain')
+        button_random = wx.Button(panel_3, wx.ID_ANY, 'Random Strain')
+        button_wave = wx.Button(panel_3, wx.ID_ANY, 'Square Wave')
+
+        button_constant.SetBackgroundColour((89, 99, 182))
+        button_constant.SetForegroundColour((255,255,255))
+        button_random.SetBackgroundColour((89, 99, 182))
+        button_random.SetForegroundColour((255,255,255))
+        button_wave.SetBackgroundColour((89, 99, 182))
+        button_wave.SetForegroundColour((255,255,255))
+
+        #button_constant.Bind(wx.EVT_BUTTON, lambda event: #on_compression_test_click(event, self.name))
+        #button_random.Bind(wx.EVT_BUTTON, lambda event: #on_tensile_test_click(event, self.name))
+
+
+        button_home = wx.BitmapButton(panel_5, wx.ID_ANY, bitmap = dashboard_img)
+        button_jobs = wx.BitmapButton(panel_5, wx.ID_ANY, bitmap = jobs_img)
+        button_settings = wx.BitmapButton(panel_5, wx.ID_ANY, bitmap = settings_img)
+
+        button_home.Bind(wx.EVT_BUTTON, lambda event: on_home_click(event, self.__class__.__name__))
+        
+        button_home.SetBackgroundColour((0, 0, 0))
+        button_jobs.SetBackgroundColour((0, 0, 0))
+        button_settings.SetBackgroundColour((0, 0, 0))
+
+        window_sizer = wx.BoxSizer(wx.VERTICAL)           #For housing entire application window 
+        middle_sizer = wx.GridSizer(1, 3, 20, 20)         #For housing middle panel
+       
+        navigation_grid_sizer = wx.GridSizer(1, 3, 0, 0)  #For housig the three navigation buttons
+
+        middle_sizer.Add(button_constant, 0, wx.EXPAND)
+        middle_sizer.Add(button_random, 0, wx.EXPAND)
+        middle_sizer.Add(button_wave, 0, wx.EXPAND)
+        
+        navigation_grid_sizer.Add(button_home, 0, wx.EXPAND)
+        navigation_grid_sizer.Add(button_jobs, 0, wx.EXPAND)
+        navigation_grid_sizer.Add(button_settings, 0, wx.EXPAND)
+        
+        panel_1.SetSizer(text_sizer_1)
+        panel_2.SetSizer(text_sizer_2)
+        panel_3.SetSizer(middle_sizer)
+        panel_4.SetSizer(text_sizer_3)
+        panel_5.SetSizer(navigation_grid_sizer)
+
+        window_sizer.Add(panel_1, 1, wx.EXPAND)
+        window_sizer.Add(panel_2, 2, wx.EXPAND)
+        window_sizer.Add(panel_3, 6, wx.EXPAND)
+        window_sizer.Add(panel_4, 2, wx.EXPAND)
+        window_sizer.Add(panel_5, 2, wx.EXPAND)
+   
+        self.SetAutoLayout(True)
+        self.SetSizer(window_sizer)
+        self.Layout()
+
+class ConstantStrainTestInput(wx.Frame):
     def __init__(self, motor_name, test_type):
         wx.Frame.__init__(self, None, size=(length, width))
 
