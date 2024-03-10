3952f0b3bc11b3efe8c8df013bd5d1c32ec8b114
nolannguyentran <nolannguyentran@u.boisestate.edu>
2024-03-09 23:31:02 -0700

    updated all four motors gpio pins

--- motor_config.py
+++ motor_config.py
@@ -14,20 +14,20 @@ import RPi.GPIO as GPIO
 
 motor_dict = {					#dictionary containing respective step/dir pin for each motor configuration
 	'A':{
+		'step_pin':38,
+		'dir_pin': 40
+	},
+	'B':{
+		'step_pin':32,
+		'dir_pin': 36
+	},
+	'C':{
 		'step_pin':33,
 		'dir_pin': 35
 	},
-	'B':{
-		'step_pin':1,
-		'dir_pin': 2
-	},
-	'C':{
-		'step_pin':3,
-		'dir_pin': 4
-	},
 	'D':{
-		'step_pin':5,
-		'dir_pin': 6
+		'step_pin':29,
+		'dir_pin': 31
 	},
 }
 
