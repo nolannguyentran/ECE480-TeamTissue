4ede7dc5e4565ba6cfa30b0721c99ba6c2d43492
Jason Pearson <jasonpearson@u.boisestate.edu>
2024-03-11 14:52:55 -0600

    WORKING LOADCELL CODE

--- loadcell_config.py
+++ loadcell_config.py
@@ -1,69 +1,63 @@
-import RPi.GPIO as GPIO
-from hx711 import HX711
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO  # import GPIO
+from hx711 import HX711  # import the class HX711
 
-# -------------lOAD CELL CONFIGURATION FILE: SET UP LOAD CELLS BASED ON THEIR GPIO PINS & IN RELATION TO MOTORS -------------------------------------------------
-#motor a = load cell a....etc
+try:
+    GPIO.setmode(GPIO.BOARD)  # set GPIO pin mode to BCM numbering
+    # Create an object hx which represents your real hx711 chip
+    # Required input parameters are only 'dout_pin' and 'pd_sck_pin'
+    hx = HX711(dout_pin=24, pd_sck_pin=26)
+    # measure tare and save the value as offset for current channel
+    # and gain selected. That means channel A and gain 128
+    err = hx.zero()
+    # check if successful
+    if err:
+        raise ValueError('Tare is unsuccessful.')
 
-loadcell_dict = {					#dictionary containing respective step/dir pin for each motor configuration
-	'A':{
-		'dout_pin':5,               #data pin
-		'pd_sck_pin': 6,            #clock pin
-		'channel': 'A',
-		'gain': 64,
-        'num_readings': 10
-	},
-	'B':{
-		'dout_pin':1,
-		'pd_sck_pin': 2,
-		'channel': 'A',
-		'gain': 64, 
-        'num_readings': 10
-	},
-	'C':{
-		'dout_pin':3,
-		'pd_sck_pin': 4,
-		'channel': 'A',
-		'gain': 64,
-        'num_readings': 10
-	},
-	'D':{
-		'dout_pin':7,
-		'pd_sck_pin': 8,
-		'channel': 'A',
-		'gain': 64,
-        'num_readings': 10
-	},
-}
+    reading = hx.get_raw_data_mean()
+    if reading:  # always check if you get correct value or only False
+        # now the value is close to 0
+        print('Data subtracted by offset but still not converted to units:',
+              reading)
+    else:
+        print('invalid data', reading)
 
-# function to initialize channel, gain, and setup pins for loadcell 1-4
-def initialize_loadcells():
-    global loadcell_A
-    global loadcell_B
-    global loadcell_C
-    global loadcell_D
+    # In order to calculate the conversion ratio to some units, in my case I want grams,
+    # you must have known weight.
+    input('Put known weight on the scale and then press Enter')
+    reading = hx.get_data_mean()
+    if reading:
+        print('Mean value from HX711 subtracted by offset:', reading)
+        known_weight_grams = input(
+            'Write how many grams it was and press Enter: ')
+        try:
+            value = float(known_weight_grams)
+            print(value, 'grams')
+        except ValueError:
+            print('Expected integer or float and I have got:',
+                  known_weight_grams)
 
-    loadcell_A = HX711(dout_pin=loadcell_dict['A']['dout_pin'], pd_sck_pin=loadcell_dict['A']['pd_sck_pin'], channel=loadcell_dict['A']['channel'], gain=loadcell_dict['A']['gain']) 
-    loadcell_B = HX711(dout_pin=loadcell_dict['B']['dout_pin'], pd_sck_pin=loadcell_dict['B']['pd_sck_pin'], channel=loadcell_dict['B']['channel'], gain=loadcell_dict['B']['gain']) 
-    loadcell_C = HX711(dout_pin=loadcell_dict['C']['dout_pin'], pd_sck_pin=loadcell_dict['C']['pd_sck_pin'], channel=loadcell_dict['C']['channel'], gain=loadcell_dict['C']['gain']) 
-    loadcell_D = HX711(dout_pin=loadcell_dict['D']['dout_pin'], pd_sck_pin=loadcell_dict['D']['pd_sck_pin'], channel=loadcell_dict['D']['channel'], gain=loadcell_dict['D']['gain']) 
+        # set scale ratio for particular channel and gain which is
+        # used to calculate the conversion to units. Required argument is only
+        # scale ratio. Without arguments 'channel' and 'gain_A' it sets
+        # the ratio for current channel and gain.
+        ratio = reading / value  # calculate the ratio for channel A and gain 128
+        hx.set_scale_ratio(ratio)  # set ratio for current channel
+        print('Ratio is set.')
+    else:
+        raise ValueError('Cannot calculate mean value. Try debug mode. Variable reading:', reading)
 
-    loadcell_A.reset()
-    loadcell_B.reset()
-    loadcell_C.reset()
-    loadcell_D.reset()
+    # Read data several times and return mean value
+    # subtracted by offset and converted by scale ratio to
+    # desired units. In my case in grams.
+    print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
+    input('Press Enter to begin reading')
+    print('Current weight on the scale in grams is: ')
+    while True:
+        print(hx.get_weight_mean(20), 'g')
 
-def read_data(motor_name):
-    match motor_name:
-        case 'A':
-            loadcell_A.get_raw_data(loadcell_dict[motor_name]['num_readings'])
-        case 'B':
-            loadcell_B.get_raw_data(loadcell_dict[motor_name]['num_readings'])
-        case 'C':
-            loadcell_C.get_raw_data(loadcell_dict[motor_name]['num_readings'])
-        case 'D':
-            loadcell_D.get_raw_data(loadcell_dict[motor_name]['num_readings'])
+except (KeyboardInterrupt, SystemExit):
+    print('Bye :)')
 
-
-#GPIO.cleanup() -----------------------------------------------------> might add this line everytime close test
-#code source: https://pypi.org/project/hx711/ 
-#code source: https://github.com/mpibpc-mroose/hx711/blob/master/example.py 
+finally:
+    GPIO.cleanup()
