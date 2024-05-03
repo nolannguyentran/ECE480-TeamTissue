ce8d5c2ef5f9f1a615eec62d41fde51d6af03ae9
Nolan Nguyen-Tran <nolannguyentran@u.boisestate.edu>
2024-05-02 23:07:54 -0600

    removed channel, gain, numreadings in loadcell config

--- loadcell_config.py
+++ loadcell_config.py
@@ -1,34 +1,22 @@
-# -------------lOAD CELL CONFIGURATION--------------------------------------------------------------------------------
+# -------------LOAD CELL CONFIGURATION--------------------------------------------------------------------------------
 # SET UP LOAD CELLS BASED ON THEIR GPIO PINS
 
 
 loadcell_dict = {					#dictionary containing respective step/dir pin for each load cell configuration
 	'A':{
 		'dout_pin': 7,               #data pin
-		'pd_sck_pin': 11,            #clock pin
-		'channel': 'A',
-		'gain': 64,
-        'num_readings': 10
+		'pd_sck_pin': 11             #clock pin
 	},
 	'B':{
 		'dout_pin':22,
-		'pd_sck_pin': 18,
-		'channel': 'B',
-		'gain': 64, 
-        'num_readings': 10
+		'pd_sck_pin': 18
 	},
 	'C':{
 		'dout_pin': 15,
-		'pd_sck_pin': 13,
-		'channel': 'C',
-		'gain': 64,
-        'num_readings': 10
+		'pd_sck_pin': 13
 	},
 	'D':{
 		'dout_pin':24,
-		'pd_sck_pin': 26,
-		'channel': 'D',
-		'gain': 64,
-        'num_readings': 10
+		'pd_sck_pin': 26
 	},
 }
